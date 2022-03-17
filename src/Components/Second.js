d1e806e3727a209445c945d68fe5c91c5733f56d
dhani <dhaniverdant23@gmail.com>
2022-03-17 10:45:31 +0800

    add some text on second page

--- src/Components/Second.js
+++ src/Components/Second.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import user from '../Assets/Images/user.jpg';
 
 const Second = () => {
   return (
-    <div id="second" style={{height: "100vh"}}>Second</div>
+    <div id="second" style={{ height: "100vh" }}>
+      <h2>Marriage</h2>
+      <p>Assalamu`alaikum Warahmatullaahi Wabarakaatuh</p>
+      <p>Maha Suci Allah yang telah menciptakan makhluk-Nya berpasang-pasangan.<br />
+        Ya Allah semoga ridho-Mu tercurah mengiringi pernikahan kami</p>
+      <div>
+        <img src={user} alt="icon" style={{ width: "180px", height: "180px" }} />
+        <img src={user} alt="icon" style={{ width: "180px", height: "180px" }} />
+      </div>
+    </div>
   );
 }
 
