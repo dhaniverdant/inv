b40afca2b32f08a4082b2d3ec3ce88d775d72ac9
dhaniverdant <dhaniverdant23@gmail.com>
2022-08-30 15:36:26 +0800

    beautify landing page

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -5,17 +5,27 @@ import Footer from './Footer';
 import {
   useParams
 } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
 import '../Assets/Styles/App.css';
 
 function Landing() {
   let { id } = useParams();
+  // const bannerImage = require('../Assets/Images/ai_lemak.JPG');
   const [open, setOpen] = useState(false);
-  const showHeader = open ? <div><Second />
+  const showHeader = open ? 
+  <div>
+    <Second />
     <Third />
-    <Footer /></div> : <header className="App-header">
-    <div>This is Inv to:</div>
-    <div>{id}</div>
-    <button onClick={() => setOpen(true)}>open</button>
+    <Footer />
+  </div> :
+  <header className="App-header">
+    <div className='intro'>Undangan Pernikahan</div>
+    <div className='our-name'>Dhani & Utari</div>
+    <div className=''>Sabtu, 24 September 2022</div>
+    <div className='to-intro'>Kepada Yth.</div>
+    <div>Bapak/Ibu/Saudara/i</div>
+    <div className='receiver-name'>{id}</div>
+    <button style={{ marginTop: "10%" }} className='btn btn-outline-light' onClick={() => setOpen(true)}>Buka Undangan</button>
   </header>;
 
   return (
