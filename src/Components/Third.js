6fe1f76366a5b365969e8c8bc5d7183bc7317b65
dhaniverdant <dhaniverdant23@gmail.com>
2022-09-06 23:38:43 +0800

    create third page

--- src/Components/Third.js
+++ src/Components/Third.js
@@ -1,8 +1,56 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../Assets/Styles/Third.css';
+import calendar from '../Assets/Images/u_calendar-alt.png';
+import clock from '../Assets/Images/u_clock-seven.png';
+import marker from '../Assets/Images/u_map-marker.png';
 
 const Third = () => {
   return (
-    <div style={{ height: "100vh", backgroundColor: "grey" }}>Third</div>
+    <div id='third' className='third-wrapper'>
+      <div className='our-agenda-title'>ACARA KAMI</div>
+      <div className='agenda-title'>Akad Nikah</div>
+      <div className='agenda-map-wrapper'>
+        <div className='agenda-wrapper'>
+          <div style={{ width: "15%" }}>
+            <img src={calendar} />
+            <img style={{ marginTop: "10px" }} src={clock} />
+            <img style={{ marginTop: "10px" }} src={marker} />
+          </div>
+          <div>
+            <div>Jum'at, 23 September 2022</div>
+            <div style={{ marginTop: "12px" }}>07.00 - 08.00</div>
+            <div style={{ marginTop: "12px" }}>Dsn. Aimual RT 00 RW 00 Lab. Kuris Lape</div>
+          </div>
+        </div>
+        <a href='https://maps.app.goo.gl/Sasy9shtwuqSPA2v7?g_st=it'
+          target="_blank"
+          type='button'
+          className='btn btn-success btn-block mt-2'
+        >Lihat Peta</a>
+      </div>
+
+      <div className='mt-4 agenda-title'>Resepsi</div>
+      <div className='agenda-map-wrapper'>
+        <div className='agenda-wrapper'>
+          <div style={{ width: "15%" }}>
+            <img src={calendar} />
+            <img style={{ marginTop: "10px" }} src={clock} />
+            <img style={{ marginTop: "10px" }} src={marker} />
+          </div>
+          <div>
+            <div>Sabtu, 24 September 2022</div>
+            <div style={{ marginTop: "12px" }}>08.30 - 12.00</div>
+            <div style={{ marginTop: "12px" }}>Balai Pertemuan Dsn. Aimual Lab. Kuris Lape</div>
+          </div>
+        </div>
+        <a href='https://maps.app.goo.gl/2hcTBexWEarecYvb8?g_st=it'
+          target="_blank"
+          type='button'
+          className='btn btn-success btn-block mt-2'
+        >Lihat Peta</a>
+      </div>
+    </div>
   );
 }
 
