8662d348389049deec71950436a3be0721da5914
dhaniverdant <dhaniverdant23@gmail.com>
2022-09-10 11:43:57 +0800

    add not found page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './Assets/Styles/App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from './Components/Landing';
+import NotFound from './Components/NotFound';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path='/:name' element={<Landing />} /> */}
+        <Route path='/' element={<NotFound />} />
         <Route path="/:id" element={<Landing />} />
       </Routes>
     </Router>
---
+++ src/Components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>OOPS, PAGE CONTENT DOES NOT EXIST :(</h1>
+    </div>
+  );
+}
+
+export default NotFound;
