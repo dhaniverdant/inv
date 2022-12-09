f3568f505477417bda1d704907f7cdc85e6c731b
dhaniverdant <dhaniverdant23@gmail.com>
2022-12-09 09:00:30 +0800

    use new bank icon

--- src/Components/Fourth.js
+++ src/Components/Fourth.js
@@ -1,23 +1,23 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../Assets/Styles/Fourth.css';
-import bca from '../Assets/Images/bca.png';
+import bca from '../Assets/Images/bank-central-asia.png';
 
 const Fourth = () => {
-  const [rekBsi] = useState(3460344548);
+  const [rekBCA] = useState(3460344548);
 
   return (
     <div className='fourth-wrapper d-flex flex-column align-items-center'>
       <div className='our-agenda-title'>Amplop Digital</div>
       <div className='bank-wrapper shadow-lg p-3 mb-4 bg-white rounded'>
-        <div className='d-flex align-items-center justify-content-center'>
-          <img src={bca} alt='bca' style={{ width: '50px', height: '42px' }} />
+        <div className='d-flex align-items-center justify-content-center mb-1'>
+          <img src={bca} alt='bca' style={{ width: '110px', height: '42px' }} />
         </div>
         <div>An. Rizkia Anjani</div>
-        <div>{rekBsi}</div>
+        <div>{rekBCA}</div>
         <button
           type='button'
-          onClick={() => { navigator.clipboard.writeText(rekBsi) }}
+          onClick={() => { navigator.clipboard.writeText(rekBCA) }}
           className='btn btn-success btn-block mt-2'
         >Copy No. Rekening</button>
       </div>
